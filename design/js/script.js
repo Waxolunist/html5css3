da6fbdc9a7b48618260eb9d405d1c04b155c4584
Waxolunist <christian.sterzl@gmail.com>
2012-03-16 07:32:28 +0100

    omepage added

--- design/js/script.js
+++ design/js/script.js
@@ -1,28 +1,93 @@
 /* Author: Christian Sterzl
-
 */
 
-$('#search a').click(function() {
-	$(this).parent().toggleClass('searchactive');
-	setTimeout(function() { $('#searchinput').focus(); }, 2000);
+$(window).bind('hashchange', function() {
+	inActivateSearch();
+	activateArea(window.location.hash);
 });
 
-$('#searchinput').blur(function() {
-	setTimeout(function() { $('#search').removeClass('searchactive'); }, 200);
-});
+function activateSearch() {
+		$('#search').addClass('searchactive');
+		setTimeout(function() { $('#searchinput').focus(); }, 2000);
+}
 
-$('#home a').click(function() {
-	$('#home-content').parent().toggleClass('active');
-});
+function inActivateSearch() {
+		$('#search').removeClass('searchactive');
+}
 
-$('#work a').click(function() {
-	$('#work-content').parent().toggleClass('active');
-});
+function activateArea(areaid) {
+	console.log(areaid);
+	if(areaid === "#search") {
+		activateSearch();
+	} else {
+		$('#content')[0].className = $('#content')[0].className.replace(/\bactivecontent.*?\b/g, '');	
+		$('.activecontent').removeClass('activecontent');
+		var activeArea = $(areaid + '-content');
+		var index = activeArea.parent().children().index(activeArea);
+		activeArea.addClass('activecontent');	
+		activeArea.parent().addClass('activecontent_' + index);
+	}
+}
 
-$('#about a').click(function() {
-	$('#about-content').parent().toggleClass('active');
-});
+function getActiveIndex() {
+		var activeArea = $('.activecontent');
+		return activeArea.parent().children().index(activeArea);
+}
 
-$('#contact a').click(function() {
-	$('#contact-content').parent().toggleClass('active');
+//0-based
+function getAreaIdByNumber(idx) {
+	return '#' + $('nav ul li:nth-child(' + (idx + 1) + ')').attr('id');
+}
+
+function getNextArea() {
+	var idx = getActiveIndex() + 1;
+	if(idx < 0) idx = 0;
+	else if(idx > 3) idx = 3;
+	return getAreaIdByNumber(idx);
+}
+
+function getLastArea() {
+	var idx = getActiveIndex() - 1;
+	if(idx < 0) idx = 0;
+	else if(idx > 3) idx = 3;
+	return getAreaIdByNumber(idx);
+}
+
+function resetAllAreas() {
+	inActivateSearch();
+	$('#content')[0].className = $('#content')[0].className.replace(/\bactivecontent.*?\b/g, '');	
+	$('.activecontent').removeClass('activecontent');
+}
+
+$(document).ready(function() {
+	console.log("Document ready");
+
+	// Reset all to default
+	resetAllAreas();
+
+	// If no target is given choose home target
+	if(window.location.hash === "") {
+		window.location.hash = "#home";
+		activateArea("#home");
+	} else if (window.location.hash === "#search") {
+		activateArea("#home");
+	}
+
+
+
+	// Prefill all forms
+	var qs = $.deparam.querystring();	
+	$.each(qs, function(k,v) {
+		$('[name=' + k + ']').val(v);	
+	});
+
+	activateArea(window.location.hash);
+	
+	//bind keys
+	$(document).bind('keydown', 'left', function() { 
+			window.location.hash = getLastArea(); 
+	});
+	$(document).bind('keydown', 'right', function() { 
+			window.location.hash = getNextArea(); 
+	});
 });
